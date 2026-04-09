902c412bb005637c77c5c318b78bb890b1c11124
polsar-luminia <262654252+polsar-luminia@users.noreply.github.com>
2026-04-08 20:18:30 -0500

    refactor(app): remove BannerCarousel, SponsoredBanner and BannerSkeleton components

--- src/components/skeletons/BannerSkeleton.tsx
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from "react-native";
-import { SkeletonBox } from "./SkeletonBox";
-
-const { width } = Dimensions.get("window");
-
-export function BannerSkeleton() {
-  return <SkeletonBox style={{ width, height: width * 0.45 }} className="rounded-none" />;
-}
