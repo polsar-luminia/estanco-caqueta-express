ca11570cb45e59f0c08a1b493228afd92556147e
polsar-luminia <262654252+polsar-luminia@users.noreply.github.com>
2026-04-12 18:17:41 -0500

    feat(sentry): DSN real + sourcemaps via getSentryExpoConfig + project react-native

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,24 @@
 import "../global.css";
 import { useEffect } from "react";
 import { Stack } from "expo-router";
-import * as Sentry from "@sentry/react-native";
-
-Sentry.init({
-  dsn: "REEMPLAZAR_CON_DSN_DE_SENTRY",
-  tracesSampleRate: 0.1,
-  enableAutoSessionTracking: true,
-  sessionTrackingIntervalMillis: 30000,
-});
 import { StatusBar } from "expo-status-bar";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Toast from "react-native-toast-message";
+import * as Sentry from "@sentry/react-native";
 import { queryClient } from "../src/lib/query-client";
 import { useAuthStore } from "../src/stores/auth";
 import { usePushNotifications } from "../src/hooks/usePushNotifications";
 import { tracker } from "../src/lib/tracker";
 import { toastConfig } from "../src/components/ToastConfig";
 import { OfflineBanner } from "../src/components/OfflineBanner";
+
+Sentry.init({
+  dsn: "https://fb7edd7b743b98e70dd924acc3eb6134@o4511209580199936.ingest.us.sentry.io/4511209582362624",
+  tracesSampleRate: 0.1,
+  enableAutoSessionTracking: true,
+  sessionTrackingIntervalMillis: 30000,
+});
 
 export default Sentry.wrap(function RootLayout() {
   const hydrate = useAuthStore((s) => s.hydrate);
